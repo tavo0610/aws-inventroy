34c7b35e720ebe67d704426e1e621c50cbdd01a6
Gustavo Valentin <gusv0610@gmail.com>
2020-09-11 12:37:36 -0500

    Fix s3 and add ec2 service

--- get_inventroy.py
+++ get_inventroy.py
@@ -17,11 +17,15 @@ print("The number of CF Distributions are: " + cf_distributions)
 
 client_s3 = boto3.client('s3')
 response = client_s3.list_buckets()
-buckets = str(
-    (sum([len(response['Buckets']) for x in response if isinstance(response['Buckets'], list)])))
+buckets = str(len(response['Buckets']))
 print("The number of S3 Buckets are: " + buckets)
 
 client_agw = boto3.client('apigateway')
 response = client_agw.get_rest_apis()
-api_gws = len(response['items'])
+api_gws = str(len(response['items']))
 print("The number of API GW are: " + api_gws)
+
+client_ec2 = boto3.client('ec2')
+response = client_ec2.describe_instances()
+ec2 = str(len(response['Reservations']))
+print("The number of EC2 are: " + ec2)
